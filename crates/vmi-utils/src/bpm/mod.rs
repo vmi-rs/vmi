5ff3c6a507711309bab02c3117437d5e02fc3f42
Petr Beneš <w.benny@outlook.com>
2026-05-11 15:17:58 +0200

    refactor(utils/bpm): change return type to ExactSizeIterator

--- crates/vmi-utils/src/bpm/mod.rs
+++ crates/vmi-utils/src/bpm/mod.rs
@@ -385,7 +385,8 @@ where
         &mut self,
         event: &VmiEvent<<Interface::Driver as VmiDriver>::Architecture>,
         key: Key,
-    ) -> Option<impl Iterator<Item = Breakpoint<Key, Tag>> + use<'_, Interface, Key, Tag>> {
+    ) -> Option<impl ExactSizeIterator<Item = Breakpoint<Key, Tag>> + use<'_, Interface, Key, Tag>>
+    {
         let (ctx, pa, view) = self.address_for_event(event)?;
         let gfn = <Interface::Driver as VmiDriver>::Architecture::gfn_from_pa(pa);
 
